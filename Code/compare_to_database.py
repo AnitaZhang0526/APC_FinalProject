0ada986ddef34d7014efd23ffd4775e6a963229c
Jordan Hamel <jh7479@princeton.edu>
2021-11-25 11:09:46 -0500

    Create class CompareToDatabase

---
+++ Code/compare_to_database.py
@@ -0,0 +1,12 @@
+# compare_to_database.py
+#
+# This class takes in peak information
+# and returns the closest match from the database.
+
+def CompareToDatabase:
+
+	# data_type = "xrd" or "ftir"
+	# peaks = 2-D array of peak locations (2-theta) and their relative intensities
+	#  ex: [[14.24, 90], [38.88, 100], ...]
+	def __init__(self, data_type, peaks):
+    	# TODO
