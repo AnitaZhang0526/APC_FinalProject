99dff3eda6f46a5a40a194176da86f03031d5aa5
Jordan Hamel <jh7479@princeton.edu>
2021-11-25 14:42:47 -0500

    Create failing tests for xrd matching

--- Tests/compare_to_database_test.py
+++ Tests/compare_to_database_test.py
@@ -26,3 +26,23 @@ def test_match_random(mock_xrd, mock_ftir):
 	CompareToDatabase('random string').match()
 	mock_xrd.assert_not_called()
 	mock_ftir.assert_not_called()
+
+def test_match_xrd_anilite():
+	# 46.28,100,32.29,65,27.86,57,Anilite,Cu7S4
+	assert(False)
+
+def test_match_xrd_barahonaite_al():
+	# 4.01,100,7.92,70,17.79,50,Barahonaite-(Al),"(Ca,Cu,Na,Fe+++,Al )12Al2(AsO4)8(OH,Cl)x•nH2O"
+	assert(False)
+
+def test_match_xrd_two_matches():
+	# Halfway between these two
+	# 4.01,100,7.92,70,17.79,50,Barahonaite-(Al),"(Ca,Cu,Na,Fe+++,Al )12Al2(AsO4)8(OH,Cl)x•nH2O"
+	# 4.01,100,7.89,70,32.38,30,Barahonaite-(Fe),"(Ca,Cu,Na,Fe+++,Al )12Fe+++2(AsO4)8(OH,Cl)x•nH2O"
+	assert(False)
+
+def test_xrd_only_one_peak():
+	assert(False)
+
+def test_xrd_only_2_peaks():
+	assert(False)
