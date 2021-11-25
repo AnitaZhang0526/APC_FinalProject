88a4866be6300026b041db64ed4ec44d67cbf374
Jordan Hamel <jh7479@princeton.edu>
2021-11-25 11:25:44 -0500

    Make failing tests for match function

--- Tests/compare_to_database_test.py
+++ Tests/compare_to_database_test.py
@@ -1,1 +1,13 @@
 # Tests for compare_to_database.py
+
+def test_match_xrd():
+	assert False
+
+def test_match_ftir():
+	assert False
+
+def test_match_none():
+	assert False
+
+def test_match_random():
+	assert False
