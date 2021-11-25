0bae4cdbc570e82e5a76538400152fc4f52588c7
Jordan Hamel <jh7479@princeton.edu>
2021-11-25 11:21:54 -0500

    Placeholder functions in compare_to_database

--- Code/compare_to_database.py
+++ Code/compare_to_database.py
@@ -9,4 +9,25 @@ def CompareToDatabase:
 	# peaks = 2-D array of peak locations (2-theta) and their relative intensities
 	#  ex: [[14.24, 90], [38.88, 100], ...]
 	def __init__(self, data_type, peaks):
-    	# TODO
+    	self.data_type = data_type
+    	self.peaks = peaks
+
+    def match():
+    	match = None;
+
+    	if data_type == 'xrd':
+    		match_xrd()
+    	else if data_type == 'ftir':
+    		match_ftir()
+    	
+		return match
+
+	# --------- Private methods ---------
+
+	def _match_xrd():
+		return 1
+
+	def _match_ftir():
+		return 1
+
+
