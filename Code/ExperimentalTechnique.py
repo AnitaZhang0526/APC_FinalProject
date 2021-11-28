295de8c4a82d511dc8d47a505cc14895b076fb81
Agnes Robang <agnesrobang@yahoo.com>
2021-11-28 15:10:56 -0500

    update ExperimentalTechnique, LeBail, Pawley, Rietveld, PeakProfileFitting to match agnes-abc branch

--- Code/ExperimentalTechnique.py
+++ Code/ExperimentalTechnique.py
@@ -2,6 +2,10 @@ import abc
 
 class ExperimentalTechnique(metaclass=abc.ABCMeta):
 	@abc.abstractmethod
+	def load_data(self):
+		pass
+
+	@abc.abstractmethod
 	def filter_baseline(self):
 		pass
 
--- Code/PeakProfileFitting.py
+++ Code/PeakProfileFitting.py
@@ -1,10 +1,4 @@
 import abc
 
 class PeakProfileFitting(metaclass=abc.ABCMeta):
-	@abc.abstractmethod
-	def fit_one_peak(self):
-		pass
-
-	@abc.abstractmethod
-	def combine_all_peaks(self):
-		pass
+	pass
