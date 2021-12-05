cc8d2e6527922c56dc872bbc64f3bc107139e9ee
arjunprihar <ap6225@princeton.edu>
2021-12-05 01:54:13 -0500

    final version complete holla

--- Code/baseline.py
+++ Code/baseline.py
@@ -1,60 +1,65 @@
-import numpy as np
-#import pandas as pd
-#import scipy.linalg as LA
-from sklearn.linear_model import LinearRegression
+#required libraries are as follows: 
+# import numpy as np
+# from sklearn.linear_model import LinearRegression
 
-#import warnings
-#warnings.filterwarnings('ignore')
-"""definitely need to cite my sources here"""
-
+"""
+BaselineRemoval method takes an array of input data and returns the data with the baseline removed. 
+BaselineRemoval uses an Ordinary Linear Regression method that fits to the QR-factorized Vandermonde matrix
+This method is based on the process outlined in "Automated Method for Subtraction of Fluorescence from Biological Raman Spectra" by Lieber et al, 2003
+This script was based on the imp
+lementation available at: https://github.com/StatguyUser/BaselineRemoval
+"""
 
 def BaselineRemoval(input_data):
         
-        vander_columns = 3
-        iterative_loops = 100
-        #error = 0.001
-        #criteria=np.inf
+        #define number of loops performed in the final linear regression to evaluate a baseline
+        iterative_loops = 100 
+      
+        """
+        A modified Vandermonde matrix is used in the subsequent linear regression to solve for a baseline.
+        The generic Vandermonde matrix forms an NxN matrix that has i^m...i^(m-1)..i^(m-m)etc. for 1<i<N entries in input_data
+        The final vandermonde_matrix is flipped horizontally, with the first column of 1's removed 
+        """
+        vander_columns = 3 #least squares linear regression in the 
+        vander_rows = np.array((list(range(1,len(input_data)+1))))
+        #vander_rows = np.array(vander_rows)      
+        vandermonde_matrix = np.fliplr(np.vander(vander_rows, vander_columns))
 
-        #building Vandermonde Matrix 
-
-        vander_rows = (list(range(1,len(input_data)+1)))
-        vander_rows = np.array(vander_rows)
-        vandermonde_matrix = np.transpose(np.vstack((vander_rows**i for i in range(vander_columns))))
-        
-        #qr factorization stuff here
-
+        """
+        QR factorization is performed to decompose the modified vandermonde matrix, A, into the product of two matricies, Q and R
+        Q is an orthogonal matrix and R is an upper triangular matrix. 
+        The Q matrix is used for fitting data to in the best-fit linear regression below. 
+        The QR factorization of the Vandermonde matrix is required for generating the coefficients used in the least squares solution
+        """
         qr_factorization = np.linalg.qr(vandermonde_matrix)[0][:,1:]
-
-        #counter=0
-        
-        #baseline ilnear regression fitting 
-        
+      
+        #define placeholders for storing iterations of computed values for the computed baseline
         working = input_data
-        old = input_data
         prediction =[]        
-        baseline=[]
-        
-        
+
+        """
+        Ordinary Least-Squares Linear Regression is performed on the input data by fitting it to the Q matrix generated above
+        LinearRegression fits a linear model to minimize the residual sum of squares between observed targets in the dataset and those predicted by the linear approximation
+        Predict is used to generate predicted labels for the trained model made by the LinearRegression().fit() method
+        """
+        #begin linear regression to evaluate baseline
         for i in range(iterative_loops):
             
-            prediction=LinearRegression().fit(qr_factorization,old).predict(qr_factorization)
-            
+            prediction=LinearRegression().fit(qr_factorization,working).predict(qr_factorization)
             working=np.array(np.minimum(input_data, prediction))
-            #criteria=sum(np.abs((working-old)/old))
-            old=working
-            #counter+=1
-        baseline=input_data-prediction
-        baseline=np.array(list(baseline))
-    
+          
+        #compute final baseline-removed data           
+        baseline=np.array(input_data-prediction)
+       
+        #return baseline-removed data 
         return baseline
     
 
-if __name__=="__main__":
-#def execute(): 
-        input_array = [1,2,5,62,24,663]
-        #input_array = np.random.randint(0, 5, 10)
-        Modpoly_output = BaselineRemoval(input_array) #ftir class hardcodes this array and creates this object 
-        #Modpoly_output=obj.ModPoly(2)
-        print('Original input:',input_array)
-        print('Modpoly base corrected values:',Modpoly_output)
+##for testing purposes, __main__ provided below: 
+# if __name__=="__main__":
+
+#          input_array = [1,2,5,62,24,663]
+#          Modpoly_output = BaselineRemoval(input_array) #ftir class hardcodes this array and creates this object 
+#          print('Original input:',input_array)
+#          print('Modpoly base corrected values:',Modpoly_output)
  
