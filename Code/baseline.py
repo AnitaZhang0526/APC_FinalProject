b431768ef88bfc35fe6d6d6364aae8ce6325fd95
arjunprihar <ap6225@princeton.edu>
2021-12-05 00:34:03 -0500

    finally it works!

--- Code/baseline.py
+++ Code/baseline.py
@@ -1,168 +1,60 @@
 import numpy as np
-import pandas as pd
-import scipy.linalg as LA
+#import pandas as pd
+#import scipy.linalg as LA
 from sklearn.linear_model import LinearRegression
 
-import math
-print("  fml ")
-import sys
+#import warnings
+#warnings.filterwarnings('ignore')
+"""definitely need to cite my sources here"""
 
 
-class BaselineRemoval():
-    '''input_array: A pandas dataframe column provided in input as dataframe['input_df_column']. It can also be a Python list object
-    degree: Polynomial degree
-    '''     
-    def __init__(self,input_array):
-        self.input_array=input_array
-        self.lin=LinearRegression()
-
-    def poly(self,input_array_for_poly,degree_for_poly):
+def BaselineRemoval(input_data):
         
-        input_array_for_poly = np.array(input_array_for_poly)
+        vander_columns = 3
+        iterative_loops = 100
+        #error = 0.001
+        #criteria=np.inf
+
+        #building Vandermonde Matrix 
+
+        vander_rows = (list(range(1,len(input_data)+1)))
+        vander_rows = np.array(vander_rows)
+        vandermonde_matrix = np.transpose(np.vstack((vander_rows**i for i in range(vander_columns))))
         
-        X = np.transpose(np.vstack((input_array_for_poly**k for k in range(degree_for_poly+1))))  #here we are adding a row, but the power is ^0 for the first set so we just get 1's 
-                
-        return np.linalg.qr(X)[0][:,1:]
+        #qr factorization stuff here
 
+        qr_factorization = np.linalg.qr(vandermonde_matrix)[0][:,1:]
 
-    def ModPoly(self,degree=2,repitition=100,gradient=0.001):
-       
-        criteria=np.inf
-
+        #counter=0
+        
+        #baseline ilnear regression fitting 
+        
+        working = input_data
+        old = input_data
+        prediction =[]        
         baseline=[]
-        corrected=[]
-
-        ywork=self.input_array
-        yold=self.input_array
-        yorig=self.input_array #probably uneccessary since we already have it saved as self.input_array
-
-        polx=self.poly(list(range(1,len(yorig)+1)),degree)
-       
-        nrep=0
-
-        while (criteria>=gradient) and (nrep<=repitition):
-            print("linfit", self.lin.fit(polx,yold)) #predict using a matrix of squaares??
-            ypred=self.lin.fit(polx,yold).predict(polx)
-            print("ypred is", ypred)
-
-            ywork=np.array(np.minimum(yorig,ypred))
-            criteria=sum(np.abs((ywork-yold)/yold))
-            yold=ywork
-            nrep+=1
-        corrected=yorig-ypred
-        corrected=np.array(list(corrected))
+        
+        
+        for i in range(iterative_loops):
+            
+            prediction=LinearRegression().fit(qr_factorization,old).predict(qr_factorization)
+            
+            working=np.array(np.minimum(input_data, prediction))
+            #criteria=sum(np.abs((working-old)/old))
+            old=working
+            #counter+=1
+        baseline=input_data-prediction
+        baseline=np.array(list(baseline))
     
-        return corrected
+        return baseline
     
 
 if __name__=="__main__":
 #def execute(): 
         input_array = [1,2,5,62,24,663]
         #input_array = np.random.randint(0, 5, 10)
-        obj = BaselineRemoval(input_array) #ftir class hardcodes this array and creates this object 
-        Modpoly_output=obj.ModPoly(2)
+        Modpoly_output = BaselineRemoval(input_array) #ftir class hardcodes this array and creates this object 
+        #Modpoly_output=obj.ModPoly(2)
         print('Original input:',input_array)
         print('Modpoly base corrected values:',Modpoly_output)
-        
-
-
-
-
-
-# banana = pd.read_excel('Book1.xlsx', converters={'wavenumber':float, 'intensity':float, 'baselineintensity':float})  
-# y= banana["baselineintensity"]
-# print("testing")
-# answer=isinstance(y[1],float)
-# print(answer)
-# print("tested")
-
-
-# #---------------actually stolen code-----
-# def baseline(y, deg=None, max_it=None, tol=None):
-
-#     """
-#     Computes the baseline of a given data.
-
-#     Iteratively performs a polynomial fitting in the data to detect its
-#     baseline. At every iteration, the fitting weights on the regions with
-#     peaks are reduced to identify the baseline only.
-
-#     Parameters
-#     ----------
-#     y : ndarray
-#         Data to detect the baseline.
-#     deg : int (default: 3)
-#         Degree of the polynomial that will estimate the data baseline. A low
-#         degree may fail to detect all the baseline present, while a high
-#         degree may make the data too oscillatory, especially at the edges.
-#     max_it : int (default: 100)
-#         Maximum number of iterations to perform.
-#     tol : float (default: 1e-3)
-#         Tolerance to use when comparing the difference between the current
-#         fit coefficients and the ones from the last iteration. The iteration
-#         procedure will stop when the difference between them is lower than
-#         *tol*.
-
-#     Returns
-#     -------
-#     ndarray
-#         Array with the baseline amplitude for every original point in *y*
-#     """
-#     # for not repeating ourselves in `envelope`
-#     if deg is None: deg = 3
-#     if max_it is None: max_it = 100
-#     if tol is None: tol = 1e-3
-    
-#     order = deg + 1
-#     coeffs = np.ones(order)
-
-#     # try to avoid numerical issues
-#     cond = math.pow(abs(y).max(), 1. / order)
-#     x = np.linspace(0., cond, y.size)
-#     base = y.copy()
-
-#     vander = np.vander(x, order)
-#     vander_pinv = LA.pinv2(vander)
-
-#     for _ in range(max_it):
-#         coeffs_new = np.dot(vander_pinv, y)
-
-#         if LA.norm(coeffs_new - coeffs) / LA.norm(coeffs) < tol:
-#             break
-
-#         coeffs = coeffs_new
-#         base = np.dot(vander, coeffs)
-#         y = np.minimum(y, base)
-
-#     return base
-
-# def envelope(y, deg=None, max_it=None, tol=None):
-#     """
-#     Computes the upper envelope of a given data.
-#     It is implemented in terms of the `baseline` function.
-    
-#     Parameters
-#     ----------
-#     y : ndarray
-#         Data to detect the baseline.
-#     deg : int
-#         Degree of the polynomial that will estimate the envelope.
-#     max_it : int
-#         Maximum number of iterations to perform.
-#     tol : float
-#         Tolerance to use when comparing the difference between the current
-#         fit coefficients and the ones from the last iteration.
-
-#     Returns
-#     -------
-#     ndarray
-#         Array with the envelope amplitude for every original point in *y*
-#     """
-#     return y.max() - baseline(y.max() - y, deg, max_it, tol)
-
-
-# fixed_data = envelope(y, deg=None, max_it=None, tol=None)
-# print(fixed_data)
-# a = np.array([[1,4,2],[7,9,4],[0,6,2]])
-# np.savetxt('myfile.csv', fixed_data, delimiter=',')
-
+ 
--- Code/baselineQR.py
+++ Code/baselineQR.py
@@ -19,17 +19,20 @@ class BaselineRemoval():   #(probably dont need a class if this is all in main,
     #     self.vander_degree  = 2
     #     #self.error = 0.001
     #     self.loops = 100
+    # def __init__(self,input_data):
+    #     self.lin=LinearRegression()
+    #     self.input_data=input_data
 
-    def BaselineRemoval(input_data):
+    def BaselineRemoval(inputdata):
 
-        input_data = np.array(input_data)
+        input_data = np.array(inputdata)
         #vander_degree  = 2
         #self.error = 0.001
         loops = 100
         print(len(input_data))
                 #make a list from 1 to N of the total # of entries in the dataset
         # length_data = list(range(1,len(input_data)))
-        vandermonde_matrix=np.vander(list(range(1,len(input_data))))
+        vandermonde_matrix=np.vander(list(range(0,len(input_data))),3)
         vandermonde_matrix=np.fliplr(vandermonde_matrix)
         
         # print(length_data)
@@ -38,207 +41,85 @@ class BaselineRemoval():   #(probably dont need a class if this is all in main,
         #     vandermonde_matrix.append(temp) #
         print('vander is',vandermonde_matrix)
 
-        print(np.linalg.qr(vandermonde_matrix))
+        print(np.linalg.qr(vandermonde_matrix)[0][:,1:])
         qr_factorized = np.linalg.qr(vandermonde_matrix)[0][:,1:]
 
         old_data = input_data
         baseline = []
         predicted_data = []
-                
 
         for i in range(loops):
-                predicted_data = LinearRegression.fit(qr_factorized,old_data).predict(qr_factorized)
-                old_data = np.data(np.minimum(input_data,predicted_data))
+                predicted_data = LinearRegression().fit(qr_factorized,old_data).predict(old_data) #LinearRegression().fit(qr_factorized,old_data).predict(qr_factorized)
+                old_data = np.array(np.minimum(input_data,predicted_data))
         baseline = old_data
         return baseline
 
 if __name__=="__main__":
-        input_data = ([35,134,1,123,42])
+        input_data = ([35,134,1,123,42,22,22])
         print(input_data)
         baseline = BaselineRemoval.BaselineRemoval(input_data)
-        print(baseline)
+        print('baseline is',baseline)
+
+import numpy as np
+#import pandas as pd
+#import scipy.linalg as LA
+from sklearn.linear_model import LinearRegression
+
+#import warnings
+#warnings.filterwarnings('ignore')
+#"""definitely need to cite my sources here"""
 
 
-    # def vandermonde(self): 
+# def BaselineRemoval(input_data):
         
-    #     vandermonde_matrix=[]
-    #     #make a list from 1 to N of the total # of entries in the dataset
-    #     length_data = list(range(1,len(self.input_data)))
-    #     for i in range(self.vander_degree+1): #why is it plus 1
-    #         temp = length_data**i
-    #         vandermonde_matrix=np.hstack([vandermonde_matrix,temp]) #
-    #         print(vandermonde_matrix)
-    #     return vandermonde_matrix
+#         vander_columns = 3
+#         iterative_loops = 100
+#         error = 0.001
+#         criteria=np.inf
 
+#         #building Vandermonde Matrix 
+
+#         vander_rows = (list(range(1,len(input_data)+1)))
+#         vander_rows = np.array(vander_rows)
+#         vandermonde_matrix = np.transpose(np.vstack((vander_rows**i for i in range(vander_columns))))
+#         qr_factorization = np.linalg.qr(vandermonde_matrix)[0][:,1:]
+
+#         counter=0
+#         working = input_data
+#         old = input_data
+#         prediction =[]        
+#         baseline=[]
         
-        #currently just returning the exact section that we want, after creating our vandermonde array
-    # def qr_factorization(vandermonde_matrix):
-    #     #need to pass my vandermonde result to this guy
-    #     print(np.linalg.qr(vandermonde_matrix))
-    #     qr_factorized= np.linalg.qr(vandermonde_matrix)[0][:,1:]
-    #     return qr_factorized
+#         # ywork=input_array
+#         # yold=input_array
+#         # yorig=input_array
+      
+#         # temp = 
+#         # input_array_for_poly = np.array(temp)
+#         # X = np.transpose(np.vstack((input_array_for_poly**k for k in range(vander_size))))
+#         # polx = np.linalg.qr(X)[0][:,1:] 
+#         # nrep=0
 
-    # def linear_regression(self,qr_factorized):
-    #     #run the whole iterative loop
-    #     #return self.input_data.LinearRegression.fit()
-    #     old_data = self.input_data
-    #     baseline = []
-    #     predicted_data = []
-        
-
-    #     for i in range(self.loops):
-    #         predicted_data = LinearRegression.fit(qr_factorized,old_data).predict(qr_factorized)
-    #         old_data = np.data(np.minimum(self.input_data,predicted_data))
-    #     baseline = old_data
-    #     return baseline
-                    
-
-
-#_____________________________________________________________________
-
-
-# #def (readfile) - to go into the ABC 
-# class BaselineRemoval():   
-
-#     def __init__(self,input_data):
-
-#         self.input_data = np.array(input_data)
-#         self.vander_degree  = 2
-#         #self.error = 0.001
-#         self.loops = 100
-
-#     def vandermonde(self): 
-        
-#         vandermonde_matrix=[]
-#         #make a list from 1 to N of the total # of entries in the dataset
-#         length_data = list(range(1,len(self.input_data)))
-#         for i in range(self.vander_degree+1): #why is it plus 1
-#             temp = length_data**i
-#             vandermonde_matrix=np.hstack([vandermonde_matrix,temp]) #
-#             print(vandermonde_matrix)
-#         return vandermonde_matrix
-
-#         #X = np.transpose(np.vstack((input_array_for_poly**k for k in range(degree_for_poly+1)))) 
-#         #count the total entries in df  our data and then use that to create an N number of values in our vandermonde
-
-
-
-#         #currently just returning the exact section that we want, after creating our vandermonde array
-#     def qr_factorization(vandermonde_matrix):
-#         #need to pass my vandermonde result to this guy
-#         print(np.linalg.qr(vandermonde_matrix))
-#         qr_factorized= np.linalg.qr(vandermonde_matrix)[0][:,1:]
-#         return qr_factorized
-
-#     def linear_regression(self,qr_factorized):
-#         #run the whole iterative loop
-#         #return self.input_data.LinearRegression.fit()
-#         old_data = self.input_data
-#         baseline = []
-#         predicted_data = []
-        
-
-#         for i in range(self.loops):
-#             predicted_data = LinearRegression.fit(qr_factorized,old_data).predict(qr_factorized)
-#             old_data = np.data(np.minimum(self.input_data,predicted_data))
-#         baseline = old_data
-#         return baseline
-                    
-# if __name__=="__main__":
-#         input_data = [1,2,5,62,24,663,33,45,35,134,1,123,42]
-#         object = BaselineRemoval(input_data)
-#         vandermonde_matrix = object.vandermonde(input_data)
-#         qr_factorized = vandermonde_matrix.qr_factorization(vandermonde_matrix)
-#         baseline = BaselineRemoval.linear_regression(qr_factorized)
-#         print(baseline)
-
-#________________________________________________________________
-
-    #remove the input data since i already passed it in init and im passing init through self
-
-        
-
-#     #describe the process of using a vandermonde matrix to perform a linear regression by fitting our data to the qr factorization of a vandermonde matrix
-
-
-# #def (readfile) - to go into the ABC 
-# class BaselineRemoval():   
-
-#     def __init__(self,input_data):
-
-#         self.input_data = np.array(input_data)
-#         self.vander_degree  = 2
-#         #self.error = 0.001
-#         self.loops = 100
-
-#     def vandermonde(self): 
-        
-#         vandermonde_matrix=[]
-#         #make a list from 1 to N of the total # of entries in the dataset
-#         length_data = list(range(1,len(self.input_data)))
-#         for i in range(self.vander_degree+1): #why is it plus 1
-#             temp = length_data**i
-#             vandermonde_matrix=np.hstack([vandermonde_matrix,temp]) #
-#             print(vandermonde_matrix)
-#         return np.transpose(vandermonde_matrix)
-
-#         #X = np.transpose(np.vstack((input_array_for_poly**k for k in range(degree_for_poly+1)))) 
-#         #count the total entries in df  our data and then use that to create an N number of values in our vandermonde
-
-
-
-#         #currently just returning the exact section that we want, after creating our vandermonde array
-#     def qr_factorization(self,vandermonde_matrix):
-#         #need to pass my vandermonde result to this guy
-#         print(np.linalg.qr(vandermonde_matrix))
-#         return np.linalg.qr(vandermonde_matrix)[0][:,1:]
-
-#     def linear_regression(self,vandermonde_matrix):
-#         #run the whole iterative loop
-#         #return self.input_data.LinearRegression.fit()
-#         old_data = self.input_data
-#         baseline = []
-#         predicted_data = []
-        
-
-#         for i in range(self.loops):
-#             predicted_data = LinearRegression.fit(vandermonde_matrix,old_data).predict(vandermonde_matrix)
-#             old_data = np.data(np.minimum(self.input_data,predicted_data))
-#         baseline = old_data
-#         return baseline
-#                     #currently not checking the error (criteria>=gradient) and (nrep<=repitition):
-#             #ypred=self.lin.fit(polx,yold).predict(polx)
+#         while (criteria>=error) and (counter<=iterative_loops):
             
-
-#             #ywork=np.array(np.minimum(yorig,ypred))
-#             #criteria=sum(np.abs((ywork-yold)/yold))
-#             #yold=ywork
-#             #nrep+=1
-#         #corrected=yorig-ypred
-#         #corrected=np.array(list(corrected))
-#         #return 0
-
-        
-#     def baseline_removal(self): #and anything else I need to do the final computation which is removing the baseline from input_array
-#         vandermonde_matrix = vandermonde(self)
-#         qr_factorized = qr_factorization(self, vandermonde_matrix)
-#         baseline = linear_regression(self,qr_factorized)
+#             prediction=LinearRegression().fit(qr_factorization,old).predict(qr_factorization)
+            
+#             working=np.array(np.minimum(input_data, prediction))
+#             criteria=sum(np.abs((working-old)/old))
+#             old=working
+#             counter+=1
+#         baseline=input_data-prediction
+#         baseline=np.array(list(baseline))
+    
 #         return baseline
+    
 
 # if __name__=="__main__":
-#         input_data = [1,2,5,62,24,663]
-#         instantiate = BaselineRemoval(input_data)
-#         baseline = instantiate.baseline_removal
-#         print(baseline)
-
-
-
-#     #remove the input data since i already passed it in init and im passing init through self
-
-        
-
-#     #describe the process of using a vandermonde matrix to perform a linear regression by fitting our data to the qr factorization of a vandermonde matrix
-
-
-
-
+# #def execute(): 
+#         input_array = [1,2,5,62,24,663]
+#         #input_array = np.random.randint(0, 5, 10)
+#         Modpoly_output = BaselineRemoval(input_array) #ftir class hardcodes this array and creates this object 
+#         #Modpoly_output=obj.ModPoly(2)
+#         print('Original input:',input_array)
+#         print('Modpoly base corrected values:',Modpoly_output)
+ 
