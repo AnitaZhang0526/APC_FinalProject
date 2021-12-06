8e0c30181c7e089139ff47cc60be04d85f47fb52
arjunprihar <ap6225@princeton.edu>
2021-12-05 22:50:06 -0500

    created a test that worked on circleCI

---
+++ Tests/baseline_test.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+from Code.baseline import baseline_removal
+import numpy as np
+
+
+def test_baseline_removal(): 
+
+    test_data = np.array([1,1,1,1,1,1,1,1,1,1])
+    answer = baseline_removal(test_data)
+    correct = np.array([0,0,0,0,0,0,0,0,0,0])
+
+    assert (answer==correct)
+
+
