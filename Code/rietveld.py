35db7ebf07e2e2dcf198af7cb8f92e27aa6d1b7b
AnitaZhang0526 <90064279+AnitaZhang0526@users.noreply.github.com>
2021-12-07 00:45:27 -0500

    re-push reitveld.py

--- Code/rietveld.py
+++ Code/rietveld.py
@@ -9,14 +9,16 @@ class Rietveld(PeakProfileFitting):
     # initialize the class
     # the class has two properties, each a float array from input file
     # x is the independent variable, and I is the depedent variable
-    def __init__(self, x, I):
-        self.x = x
-        self.I = I
+    def __init__(self, cutoff,peak_widths,spectrum):
+        self.cutoff = cutoff
+        self.peak_width = peak_widths
+        self.x = spectrum['x']
+        self.I = spectrum['y']
 
-    def get_peaks(self,cutoff,peak_widths):
-        b,a = signal.butter(2, cutoff, 'low')
+    def get_peaks(self):
+        b,a = signal.butter(2, self.cutoff, 'low')
         I_filtered = signal.filtfilt(b,a,self.I)
-        peak_indicies = signal.find_peaks_cwt(I_filtered, peak_widths)
+        peak_indicies = signal.find_peaks_cwt(I_filtered, self.peak_widths)
         threshold = 0.20
         idx =(self.I[peak_indicies]>threshold)
         peak_indicies = peak_indicies[idx]
@@ -26,16 +28,17 @@ class Rietveld(PeakProfileFitting):
         c = np.sum(np.power(results-self.I, 2))/len(self.x)
         return c
 
-    def make_spec(self,peak_widths,model_choices,peak_indices):
+    def make_spec(self,model_choices):
         modelType = []
         height = []
         sigma = []
         center = []
         x_range = np.max(self.x)-np.min(self.x)
+        peak_indices = self.get_peaks()
         for model_idx,peak_idx in enumerate(peak_indices):
             modelType.append(model_choices[model_idx])
             height.append(self.I[peak_idx])
-            sigma.append(x_range/len(self.x)*np.min(peak_widths)),
+            sigma.append(x_range/len(self.x)*np.min(self.peak_widths)),
             center.append(self.x[peak_idx])
         spec = pd.DataFrame({'modelType':modelType,
                              'height':height,
@@ -75,9 +78,9 @@ class Rietveld(PeakProfileFitting):
             else:
                 composite_model = composite_model + model
         return composite_model, params
-    
-    def find_best_fit(self,cutoff,peak_widths):
-        peak_indices = self.get_peaks(cutoff,peak_widths)
+
+    def find_best_fit(self):
+        peak_indices = self.get_peaks(self.cutoff,self.peak_widths)
         options = ['GaussianModel','LorentzianModel','VoigtModel']
         n_trials = len(options)
         lowest_cost = np.inf
@@ -85,7 +88,7 @@ class Rietveld(PeakProfileFitting):
         best_values = []
         for i in range(n_trials):
             model_choices = [options[i]]*len(peak_indices)
-            spec = self.make_spec(peak_widths,model_choices,peak_indices)
+            spec = self.make_spec(self.peak_widths,model_choices,peak_indices)
             composite_model,params = self.make_one_model(spec)
             predicted_model = composite_model.fit(self.I, params, x=self.x)
             results = predicted_model.eval(params=params)
@@ -96,11 +99,11 @@ class Rietveld(PeakProfileFitting):
                 best_values = predicted_model.best_values
         return best_model_choices, best_values
 
-    def get_params(self,cutoff,peak_widths):
+    def get_params(self):
         intensity = []
         center = []
         FWHM = []
-        model_choices, values = self.find_best_fit(cutoff,peak_widths)
+        model_choices, values = self.find_best_fit(self.cutoff,self.peak_widths)
         for i in range(len(model_choices)):
             prefix = f'm{i}_'
             key_amp = prefix+'amplitude'
