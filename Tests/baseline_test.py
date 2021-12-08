93185311687068288e845f9d5c87f9a486560fab
arjunprihar <ap6225@princeton.edu>
2021-12-07 19:52:49 -0500

    trying to fix file referencing error

--- Tests/baseline_test.py
+++ Tests/baseline_test.py
@@ -1,6 +1,6 @@
 import unittest
 import sys
-from Code.baseline import baseline_removal
+from baseline import baseline_removal
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
@@ -9,8 +9,13 @@ def test_baseline_removal():
 
     test_data = np.array([1,1,1,1,1,1,1,1,1,1])
     answer = baseline_removal(test_data)
+    print(answer)
     correct = np.array([0,0,0,0,0,0,0,0,0,0])
 
-    assert (answer==correct)
+    return np.testing.assert_array_equal(answer, correct, 'not equal bad bad bad')
 
 
+if __name__=="__main__":
+    
+        answer = test_baseline_removal()
+        print(answer)
