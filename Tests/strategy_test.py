8b1c4679aa6795b218db8235df53227022a48c54
Anita Zhang <yz3655@princeton.edu>
2021-12-11 17:24:47 -0500

    oops I forgot what I and x should be rip

--- Tests/strategy_test.py
+++ Tests/strategy_test.py
@@ -6,8 +6,8 @@ def test_make_one_spec():
     peak_indices = [11, 121, 255, 328, 582, 731, 802, 1020, 1104, 1397, 1674]
     strategy = Strategy()
     model_choices = ['GaussianModel']*11
-    I = np.random.rand(11)
-    x = [i * 0.05 for i in peak_indices]
+    I = np.random.rand(1800)
+    x = np.arange(0,90,0.05)
     peak_widths = np.arange(5,15)
     spec = strategy.make_one_spec(model_choices,peak_indices,I,x,peak_widths)
     assert(spec['modelType'] == model_choices)
@@ -17,8 +17,8 @@ def test_make_specs():
     strategy = Strategy()
     strategy_choice = 'best'
     peak_indices = [11, 121, 255, 328, 582, 731, 802, 1020, 1104, 1397, 1674]
-    I = np.random.rand(11)
-    x = [i * 0.05 for i in peak_indices]
+    I = np.random.rand(1800)
+    x = np.arange(0,90,0.05)
     peak_widths = np.arange(5,15)
     specs,model_choices_list = strategy.make_specs(strategy_choice,peak_indices,I,x,peak_widths)
     assert(len(specs) == 78)
