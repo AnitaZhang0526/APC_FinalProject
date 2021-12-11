f2151e8108c63b21d140c7746105d39a12b283aa
arjunprihar <ap6225@princeton.edu>
2021-12-11 18:15:25 -0500

    updated test files

--- Tests/baseline_test.py
+++ Tests/baseline_test.py
@@ -1,21 +1,45 @@
 import unittest
 import sys
-from baseline import baseline_removal
+import baseline #from baseline import baseline_removal, linear_regression, vandermonde_matrix
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
 
 def test_baseline_removal(): 
 
+    #basic test 1, method should be able to return zero baseline
     test_data = np.array([1,1,1,1,1,1,1,1,1,1])
     answer = baseline_removal(test_data)
-    print(answer)
     correct = np.array([0,0,0,0,0,0,0,0,0,0])
 
-    return np.testing.assert_array_equal(answer, correct, 'not equal bad bad bad')
+    np.testing.assert_array_equal(answer, correct)
 
+def test_vandermonde_matrix():
+
+    test_data = np.ones(5)
+    answer = vandermonde_matrix(test_data)
+    correct = [[1,1,1],[1,2,4],[1,3,9],[1,4,16],[1,5,25]]
+    
+    np.testing.assert_array_equal(answer, correct)
+
+def test_qr_factorization():
+
+    test_data = [[1,1,1],[1,2,4],[1,3,9],[1,4,16],[1,5,25]]
+    answer = vandermonde_matrix(test_data)
+    correct = [[-6.32455532e-01,5.34522484e-01],[-3.16227766e-01,-2.67261242e-01],[8.32667268e-17,-5.34522484e-01],[3.16227766e-01,-2.67261242e-01],[6.32455532e-01,5.34522484e-01]]
+
+    np.testing.assert_array_equal(answer, correct)
+
+def test_linear_regression():
+
+    test_data = np.ones(5)
+    qr_factorized = [[-6.32455532e-01,5.34522484e-01],[-3.16227766e-01,-2.67261242e-01],[8.32667268e-17,-5.34522484e-01],[3.16227766e-01,-2.67261242e-01],[6.32455532e-01,5.34522484e-01]]
+    answer = linear_regression(test_data, qr_factorized)
+    correct = np.ones(5)
+
+    np.testing.assert_array_equal(answer, correct)
 
 # if __name__=="__main__":
     
 #         answer = test_baseline_removal()
-#         print(answer)
+#        print(answer)
