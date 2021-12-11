c6ed447d546bf3d39e4f42d4f1f4765b3f7c93e4
Anita Zhang <yz3655@princeton.edu>
2021-12-11 17:08:34 -0500

    syntax error fix

--- Tests/strategy_test.py
+++ Tests/strategy_test.py
@@ -7,7 +7,7 @@ def test_make_one_spec():
     strategy = Strategy()
     model_choices = ['GaussianModel']*11
     I = np.random.rand(11)
-    x = peak_indices*0.05
+    x = [i * 0.05 for i in peak_indices]
     peak_widths = np.arange(5,15)
     spec = strategy.make_one_spec(model_choices,peak_indices,I,x,peak_widths)
     assert(spec['modelType'] == model_choices)
@@ -18,7 +18,7 @@ def test_make_specs():
     strategy_choice = 'best'
     peak_indices = [11, 121, 255, 328, 582, 731, 802, 1020, 1104, 1397, 1674]
     I = np.random.rand(11)
-    x = peak_indices*0.05
+    x = [i * 0.05 for i in peak_indices]
     peak_widths = np.arange(5,15)
     specs,model_choices_list = strategy.make_specs(strategy_choice,peak_indices,I,x,peak_widths)
     assert(len(specs) == 78)
