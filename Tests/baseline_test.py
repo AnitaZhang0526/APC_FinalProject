f4ac6b79d8bb9df9f135a9eb87f7da9e63169c1a
arjunprihar <ap6225@princeton.edu>
2021-12-11 21:01:50 -0500

    trying to fix circle-CI issue

---
+++ Tests/baseline_test.py
@@ -0,0 +1,52 @@
+
+# #for testing purposes, __main__ provided below: 
+# if __name__=="__main__":
+
+#         input_array = [1,1,1,1,1]
+#         Modpoly_output = baseline_removal(input_array) #ftir class hardcodes this array and creates this object 
+#         print('Original input:',input_array)
+#         print('Modpoly base corrected values:',Modpoly_output)
+ 
+
+import unittest
+import sys
+from Code.baseline import baseline_removal, vandermonde_matrix,qr_factorization,linear_regression 
+from sklearn import linear_model # import LinearRegression
+import numpy as np
+
+
+def test_baseline_removal(): 
+
+    #basic test 1, method should be able to return zero baseline
+    test_data = np.array([1,1,1,1,1,1,1,1,1,1])
+    answer = baseline_removal(test_data)
+    correct = np.array([0,0,0,0,0,0,0,0,0,0])
+
+    np.testing.assert_array_equal(answer, correct)
+
+def test_vandermonde_matrix():
+
+    test_data = np.ones(5)
+    answer = vandermonde_matrix(test_data)
+    correct = [[1,1,1],[1,2,4],[1,3,9],[1,4,16],[1,5,25]]
+    
+    np.testing.assert_array_equal(answer, correct)
+
+def test_qr_factorization():
+
+    test_data = [[1,1,1],[1,2,4],[1,3,9],[1,4,16],[1,5,25]]
+    answer = qr_factorization(test_data)
+    correct = [[-6.32455532e-01,5.34522484e-01],[-3.16227766e-01,-2.67261242e-01],[8.32667268e-17,-5.34522484e-01],[3.16227766e-01,-2.67261242e-01],[6.32455532e-01,5.34522484e-01]]
+
+    np.testing.assert_array_equal(answer, correct)
+
+def test_linear_regression():
+
+    test_data = np.ones(5)
+    qr_factorized = [[-6.32455532e-01,5.34522484e-01],[-3.16227766e-01,-2.67261242e-01],[8.32667268e-17,-5.34522484e-01],[3.16227766e-01,-2.67261242e-01],[6.32455532e-01,5.34522484e-01]]
+    answer = linear_regression(test_data, qr_factorized)
+    correct = np.ones(5)
+
+    np.testing.assert_array_equal(answer, correct)
+
+# if __name__=="__main__":
