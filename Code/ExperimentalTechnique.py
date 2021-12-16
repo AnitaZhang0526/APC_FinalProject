a638b2340392590b4d67bff62da915498c94e94f
Agnes Robang <agnesrobang@yahoo.com>
2021-12-16 10:24:39 -0500

    deleted extraneous code

--- Code/ExperimentalTechnique.py
+++ Code/ExperimentalTechnique.py
@@ -5,11 +5,3 @@ import numpy as np
 class ExperimentalTechnique():
 	def __init__(self, spectrum):
 		self.spectrum = spectrum
-
-	### not useful because you need spectrum to declare ExperimentalTechnique in the first place
-	# def load_data(self, dir, filename):
-	# 	input_path = os.path.join(dir, 'Input', str(filename))
-	# 	spectrum = pd.read_csv(input_path, skiprows=2, header=None, names=['x','y'])
-	# 	spectrum['y']=spectrum['y']/max(spectrum['y'])
-
-	# 	return spectrum
