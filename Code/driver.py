709fc9753bf1020e624be35c193419d00c9dfb8e
Agnes Robang <agnesrobang@yahoo.com>
2021-12-17 16:35:04 -0500

    fixed conflicting argument error

--- Code/driver.py
+++ Code/driver.py
@@ -12,7 +12,7 @@ from Code.PeakProfileFitting_Factory import PeakProfileFitting_Factory as PPF_Fa
 from Code.strategy import Strategy
 from Code.compare_to_database import CompareToDatabase
 
-# Test from root directory using `python Code/driver.py -d FTIR -m "polyfit" -t -i 1-1-4-11_pH0_3-17-2020.csv`
+# Test from root directory using `python Code/driver.py -d FTIR -m "Rietveld" -f "fast" -t -i 1-1-4-11_pH0_3-17-2020.csv`
 parser = argparse.ArgumentParser(description='Analyzes results from XRD and FTIR output data.')
 parser.add_argument('-d', '--data', type=str, 
     help='Type of data being uploaded, "XRD" or "FTIR".')
@@ -25,7 +25,7 @@ parser.add_argument('-c', '--cutoff', type=str,
     help='Cutoff to be used for fitting (e.g. 0.9).')
 parser.add_argument('-r', '--range', type=str,
     help='Peak widths range to be used for fitting (e.g. "5,15").')
-parser.add_argument('-h', '--threshold', type=str,
+parser.add_argument('-s', '--threshold', type=str,
     help='Threshold for what counts as a peak (e.g. 0.2).')
 parser.add_argument('-i', '--inputfile', type=str,
     help='Filename of input file to be analyzed (e.g. "1-1-4-11_pH0_3-17-2020.csv").')
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         peak_widths = np.arange(int(peak_widths_range[0]),int(peak_widths_range[1]))
 
         # (2) Calculates the peaks and its parameters from the input data and exports this into a CSV.
-        peaks, analysis = PPF_Factory.factory_method(args['method'], args['fitting'], cutoff, peak_widths, spectrum, strategy,threshold)
+        peaks, analysis = PPF_Factory.factory_method(args['method'], args['fitting'], cutoff, peak_widths, spectrum, strategy, threshold)
         
         if not (os.path.isdir(os.path.join(dir, 'Output'))):
             os.mkdir(os.path.join(dir, 'Output'))
