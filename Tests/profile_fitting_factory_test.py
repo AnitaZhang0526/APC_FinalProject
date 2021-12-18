9cca7f8c82497766099159cb9b562aaa516117e9
Anita Zhang <yz3655@princeton.edu>
2021-12-18 17:44:35 -0500

    another bug fix

--- Tests/profile_fitting_factory_test.py
+++ Tests/profile_fitting_factory_test.py
@@ -1,5 +1,5 @@
 import pytest
-from Code.peak_profile_fitting_factory import PeakProfileFitting_Factory
+from Code.peak_profile_fitting_factory import PeakProfileFitting_Factory as PPF_factory
 from Code.rietveld import Rietveld
 from Code.poly import Poly
 import numpy as np
@@ -14,29 +14,26 @@ I = I/max(I)
 spectrum = pd.DataFrame({'x':x,'y':I})
 
 def test_poly_factory():
-    f = PeakProfileFitting_Factory()
     method = 'Rietveld'
     strategy_choice = 'fast'
     peak_widths = np.arange(5,15)
     cutoff = 0.9
     strategy = Strategy()
     threshold = 0.2
-    r = f.factory_method(method, strategy_choice, cutoff, peak_widths, spectrum, strategy, threshold)
+    r = PPF_factory.factory_method(method, strategy_choice, cutoff, peak_widths, spectrum, strategy, threshold)
     assert(isinstance(r,Rietveld))
     
 def test_Rietveld_facotry():
-    f = PeakProfileFitting_Factory()
     method = 'polyfit'
     strategy_choice = 'fast'
     peak_widths = np.arange(5,15)
     cutoff = 0.9
     strategy = Strategy()
     threshold = 0.2
-    p = f.factory_method(method, strategy_choice, cutoff, peak_widths, spectrum, strategy, threshold)
+    p = PPF_factory.factory_method(method, strategy_choice, cutoff, peak_widths, spectrum, strategy, threshold)
     assert(isinstance(p,Poly))
     
 def test_error_type():
-    f = PeakProfileFitting_Factory()
     method = 'hello'
     strategy_choice = 'fast'
     peak_widths = np.arange(5,15)
@@ -44,5 +41,5 @@ def test_error_type():
     strategy = Strategy()
     threshold = 0.2
     with pytest.raises(ValueError):
-        f.factory_method(method, strategy_choice, cutoff, peak_widths, spectrum, strategy, threshold)
+        PPF_factory.factory_method(method, strategy_choice, cutoff, peak_widths, spectrum, strategy, threshold)
     
