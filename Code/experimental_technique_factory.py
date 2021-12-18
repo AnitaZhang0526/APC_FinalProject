525b93ccd94a2928c9a0c682eaca66dc444fb9b0
Anita Zhang <yz3655@princeton.edu>
2021-12-18 18:36:09 -0500

    oops something i forgot to fix

--- Code/experimental_technique_factory.py
+++ Code/experimental_technique_factory.py
@@ -20,8 +20,8 @@ class ExperimentalTechnique_Factory():
         elif input_type == 'FTIR':
             ftir = FTIR() # create a FTIR object if the input type is FTIR
             spectrum = ftir.load_data(inputfile) # extract data and create spectrum dataFrame using the load_data method in FTIR class
-            spectrum = FTIR.flip_input(transmittance,spectrum) # flip data if y axis is transmittance instead of absorbance
-            spectrum = FTIR.filter_baseline(spectrum) # filter baseline
+            spectrum = ftir.flip_input(transmittance,spectrum) # flip data if y axis is transmittance instead of absorbance
+            spectrum = ftir.filter_baseline(spectrum) # filter baseline
             return spectrum, ftir
         else:
             raise ValueError(f'Cannot make: {input_type}') # error if the input type is not recognized
