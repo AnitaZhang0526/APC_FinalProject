2adf3297383f82d69304f085fc24066efdfa6028
Anita Zhang <yz3655@princeton.edu>
2021-12-18 16:10:01 -0500

    well idk why CI hates my indentation but i tried to fix it

--- Code/peak_profile_fitting.py
+++ Code/peak_profile_fitting.py
@@ -1,21 +1,21 @@
 import abc
 
 class PeakProfileFitting(metaclass=abc.ABCMeta):
-	"""
-	:This class is the parent class for all profile fitting methods
- 
-	:x: the independent variable data
-	:I: the intensity or the dependent variable data
-	"""
-	def __init__(self,spectrum):
+    """
+    :This class is the parent class for all profile fitting methods
+    
+    :x: the independent variable data
+    :I: the intensity or the dependent variable data
+    """
+    def __init__(self,spectrum):
         """
         :Constructor method
         :param spectrum: dataFrame containing x and y values
         """
-		self.x = spectrum['x']
-		self.I = spectrum['y']
-
-	# not implemented, abstract get_peaks_params methods
-	@abc.abstractmethod
-	def get_peaks_params(self):
-		pass
+        self.x = spectrum['x']
+        self.I = spectrum['y']
+    
+    # not implemented, abstract get_peaks_params methods 
+    @abc.abstractmethod
+    def get_peaks_params(self):
+        pass
